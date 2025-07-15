d867e777ee2833523e051c222c4a671f6db3fc6b
tarannumtasfia <tasfiatarannum@yahoo.com>
2025-07-15 17:19:54 +0600

    commited

--- src/app/contact/page.js
+++ src/app/contact/page.js
@@ -2,8 +2,8 @@
 
 export default function Contact() {
   return (
-    <main className="h-screen bg-gradient-to-br from-sky-50 to-indigo-100 flex flex-col items-center justify-start overflow-hidden">
-      
+    <main className="h-[calc(100vh-4rem)] bg-gradient-to-br from-sky-50 to-indigo-100 flex flex-col items-center justify-start overflow-hidden mt-16">
+      <h1 className="text-3xl font-bold text-indigo-800 mb-4">Contact Me</h1>
 
       <div className="w-full flex-1 overflow-hidden px-6 pb-6">
         <iframe
